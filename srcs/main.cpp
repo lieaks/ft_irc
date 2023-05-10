518fc9ea967ef7b9b3425ce7e89d9da573f3e7cc
David LY <dly@e2r6p14.clusters.42paris.fr>
2023-05-10 19:44:01 +0200

    save

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@ int main(int ac, char **av)
 	(void)av;
 	if (ac != 3)
 	{
-		std::cerr << "Usage: ./irc <port> <username" << std::endl;
+		std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
 		return 1;
 	}		
 };
