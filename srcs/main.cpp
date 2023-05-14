171666e7e17ed492bc4fdb87d2f43eb35609650c
mirsella <mirsella@protonmail.com>
2023-05-15 00:35:56 +0200

    refactor main.cpp

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,21 +4,18 @@
 
 int main(int ac, char **av)
 {
-	(void)av;
-	std::stringstream ss;
-	int port = 0;
 	if (ac != 3)
-	{
-		std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
-		return 1;
-	};
-	ss << av[1];
-	ss >> port;
+		return (std::cerr << "Usage: ./ircserv <port> <password>" << std::endl, 1);
+
+	int port = 0;
+	std::stringstream(av[1]) >> port;
+	std::cout << "port: " << port << std::endl;
+
 	try {
 		Server irc(port, av[2]);
 		irc.run_server();
 	}
-	catch (const std::exception &e){
+	catch (const std::exception &e) {
 		std::cerr << e.what() << std::endl;
 	}
 };
