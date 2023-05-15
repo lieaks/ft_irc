2ffe1d062d5b7da1ae34e8ea32d65a93e3b45fc5
mirsella <mirsella@protonmail.com>
2023-05-15 18:45:32 +0200

    improve port parsing

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <cerrno>
 #include "../includes/Server.hpp"
 
 int main(int ac, char **av)
@@ -7,11 +9,18 @@ int main(int ac, char **av)
 	if (ac != 3)
 		return (std::cerr << "Usage: ./ircserv <port> <password>" << std::endl, 1);
 
-	int port = 0;
-	std::stringstream(av[1]) >> port;
-	std::cout << "port: " << port << std::endl;
+	char *ptr;
+	errno = 0;
+	long port = std::strtol(av[1], &ptr, 10);
+	if (ptr != av[1] + std::strlen(av[1]) || std::strlen(av[1]) == 0)
+		return (std::cerr << "Error: Invalid port \"" << av[1] << "\": not a number" << std::endl, 1);
+	else if (errno != 0)
+		return (std::cerr << "Error: Invalid port \"" << av[1] << "\": " << strerror(errno) << std::endl, 1);
+	else if (port < 0 || port > 65535)
+		return (std::cerr << "Error: Invalid port \"" << av[1] << "\": must be between 0 and 65535" << std::endl, 1);
 
 	try {
+		std::cout << "port: " << port << std::endl;
 		Server irc(port, av[2]);
 		irc.run_server();
 	}
