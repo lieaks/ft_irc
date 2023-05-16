9e5ad56cb0bdcf522c036ec853f2a897c8c62cf2
David LY <dly@e3r11p6.clusters.42paris.fr>
2023-05-16 17:46:19 +0200

    numerics replies

--- includes/define.hpp
+++ includes/define.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#define	SERVER_NAME chatastrophe
 #define BACKLOG 10 //max length of the queue
 #define max_clients 10
 #define MAX_EVENTS 10
 #define BUFFER_SIZE 1024
+
+// Reponses numeriques
+// 401
+#define ERR_NOSUCHNICK(nickname, other_nickname) (":" + SERVER_NAME + " 401 " + nickname + " " + other_nickname + ": No such nick/channel")
