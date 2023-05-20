094099e7593305b85b2c26dc8c3d25dffa8b3d63
mirsella <mirsella@protonmail.com>
2023-05-20 17:24:39 +0200

    add std::string(":") in front of every define

---
+++ srcs/commands/invite.cpp
@@ -0,0 +1,13 @@
+#include "../../includes/commands.hpp"
+
+bool	cmd_invite(Server &server, Client &client, std::vector<std::string> input) {
+	if (input.size() < 2) {
+		client.send_message(ERR_NEEDMOREPARAMS(client.getNickname(), "INVITE"));
+		return false;
+	}
+	Client	*target = server.getClient(input[1]);
+	if (!target) {
+		client.send_message(ERR_NOSUCHNICK(client.getNickname(), input[1]));
+		return false;
+	}
+}
