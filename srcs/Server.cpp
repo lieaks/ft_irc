17fbf9a0057b5ad5a284b2691776815e88541242
davidlejaune <david_ly@hotmail.fr>
2023-05-20 22:57:07 +0200

    wip privmsg

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -200,10 +200,7 @@ void	Server::_init_commands( void ) {
 	_commands.insert(std::pair<std::string, bool (*)(Server&, Client&, std::vector<std::string>&)>("info", &cmd_info));
 	_commands.insert(std::pair<std::string, bool (*)(Server&, Client&, std::vector<std::string>&)>("version", &cmd_version));
 	_commands.insert(std::pair<std::string, bool (*)(Server&, Client&, std::vector<std::string>&)>("time", &cmd_time));
-<<<<<<< HEAD
 	_commands.insert(std::pair<std::string, bool (*)(Server&, Client&, std::vector<std::string>&)>("oper", &cmd_oper));
-=======
 	_commands.insert(std::pair<std::string, bool (*)(Server&, Client&, std::vector<std::string>&)>("NOTICE", &cmd_notice));
 	_commands.insert(std::pair<std::string, bool (*)(Server&, Client&, std::vector<std::string>&)>("PRIVMSG", &cmd_privmsg));
->>>>>>> 5dbe01b (wip privmsg)
 }
