6e3821ad725658015505456f0773d95a5d006eb6
mirsella <mirsella@protonmail.com>
2023-05-21 18:10:48 +0200

    finish /quit, need testing

--- srcs/commands/quit.cpp
+++ srcs/commands/quit.cpp
@@ -5,7 +5,7 @@ bool	cmd_quit(Server &server, Client &client, std::vector<std::string> &input) {
 	std::vector<Channel *>::iterator channels = client.getChannels().begin();
 	while (channels != client.getChannels().end()) {
 		/* TODO: send notice to channels users about the user quitting */
-		/* (*channels)->send_message(RPL_QUIT(client.getNickname(), input[1])); */
+		(*channels)->send_message(RPL_NOTICE(client.getNickname(), input[1]));
 	}
 	server.removeClient(server.getClient(client.getNickname()));
 	return true;
