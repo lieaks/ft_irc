8f70317eb79e6d186d5f6b728a3323cd11fa346f
Rukkyaa <lbrt.axel@gmail.com>
2023-05-21 12:05:41 +0200

    The project compile again

--- srcs/commands/notice.cpp
+++ srcs/commands/notice.cpp
@@ -2,6 +2,9 @@
 
 bool	cmd_notice(Server &server, Client &client, std::vector<std::string> &input)
 {
+	(void)server;
+	(void)client;
+	(void)input;
 	
 	return true;
 }
--- srcs/commands/privmsg.cpp
+++ srcs/commands/privmsg.cpp
@@ -2,7 +2,8 @@
 
 bool	cmd_privmsg(Server &server, Client &client, std::vector<std::string> &input)
 {
-	if (input.size() < 2 || input.size() == 2 && input[1][0] != ':')
+	(void)server;
+	if (input.size() < 2 || (input.size() == 2 && input[1][0] != ':'))
 	{
 		client.send_message(ERR_NEEDMOREPARAMS(client.getNickname(), input[0]));
 		return false;
