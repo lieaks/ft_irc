0b3215ca4f55da4f7445b58f4a81856b55977490
Axel LAMBERT <lbrt.axel@gmail.com>
2023-05-22 16:27:21 +0200

    Wallops fix|

--- srcs/commands/wallops.cpp
+++ srcs/commands/wallops.cpp
@@ -10,10 +10,18 @@ bool	cmd_wallops(Server &server, Client &client, std::vector<std::string> &input
 		return false;
 	}
 	std::map<int, Client *>::iterator it = server.getClients().begin();
+	std::string	full_msg;
+
+	for (size_t i = 1; i < input.size(); i++) {
+		full_msg += input[i];
+		if (i + 1 < input.size())
+			full_msg += " ";
+	}
+	if (full_msg[0] == ':')
+		full_msg.erase(0, 1);
 	while (it != server.getClients().end()) {
 		if (it->second->isModeSet(WALLOPS)) {
-			it->second->send_message(":" + client.getNickname() + " WALLOPS :" + input[1] + "\r\n");
-			std::cout << it->second->getNickname() << std::endl;
+			it->second->send_message(":" + client.getNickname() + " WALLOPS :" + full_msg + "\r\n");
 		}
 		++it;
 	}
