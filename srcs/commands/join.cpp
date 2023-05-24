4d1820cce5d310c435a96dc1bb9df94d8bff7959
mirsella <mirsella@protonmail.com>
2023-05-24 17:13:11 +0200

    join.cpp: call /names at join to inform client of connected users

--- srcs/commands/join.cpp
+++ srcs/commands/join.cpp
@@ -17,12 +17,10 @@ bool	cmd_join(Server &server, Client &client, std::vector<std::string> &input) {
 		client.send_message(ERR_NOTREGISTERED(client.getNickname(), input[0]));
 		return false;
 	}
-	if (input[1][0] == '#')
-		input[1] = input[1].substr(1);
-	Channel *channel = server.getChannelByName(input[1]);
+	Channel *channel = server.getChannelByName(input[1].substr(1));
 	if (channel == NULL)
 	{
-		channel = new Channel(input[1], server, &client);
+		channel = new Channel(input[1].substr(1), server, &client);
 		server.addChannel(channel);
 	}
 	else
@@ -49,6 +47,7 @@ bool	cmd_join(Server &server, Client &client, std::vector<std::string> &input) {
 		client.send_message(RPL_TOPIC(client.getNickname(), channel->getName(), channel->getTopic()));
 	else
 		client.send_message(RPL_NOTOPIC(client.getNickname(), channel->getName()));
+	cmd_names(server, client, input);
 	// client.send_message(RPL_NAMREPLY(client.getNickname(), channel->getName(), channel->getNamesClients()));
 	// client.send_message(RPL_ENDOFNAMES(client.getNickname(), channel->getName()));
 	return true;
